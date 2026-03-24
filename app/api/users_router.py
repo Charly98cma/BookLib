a841a72446da4322bd80d077e427e0d42b1213e0
Charly98cma <1998cma@gmail.com>
2026-03-24 17:43:03 +0000

    Reworked User router and added Annotations for type checking

--- app/api/users_router.py
+++ app/api/users_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-from typing import Optional, List
+from typing import Optional, List, Annotated
 from http import HTTPStatus
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.database import get_db
 from schemas.users_schema import UserCreate, UserLogin, UserDBResponse
@@ -14,50 +14,57 @@ router = APIRouter(
 
 # CREATE #######################################################################
 
-@router.post("", status_code=HTTPStatus.OK, response_model=UserDBResponse)
+@router.post("",
+             status_code=HTTPStatus.OK,
+             response_model=UserDBResponse)
 async def create_user(
     data: UserCreate,
-    session: AsyncSession = Depends(get_db)
+    session: Annotated[AsyncSession, Depends(get_db)]
 ):
     _service = UserService(session)
     return await _service.create(data)
 
 # READ #########################################################################
 
-@router.get("", status_code=HTTPStatus.OK, response_model=List[Optional[UserDBResponse]])
+@router.get("",
+            status_code=HTTPStatus.OK,
+            response_model=List[Optional[UserDBResponse]])
 async def get_all(
-    session: AsyncSession = Depends(get_db)
+    session: Annotated[AsyncSession, Depends(get_db)]
 ):
     _service = UserService(session)
     return await _service.read_all()
 
 # UPDATE #######################################################################
 
-@router.put("/{username}", status_code=HTTPStatus.OK, response_model=UserDBResponse)
+@router.put("/login",
+             status_code=HTTPStatus.OK,
+             response_model=UserDBResponse)
+async def login(
+        data: UserLogin,
+        session: Annotated[AsyncSession, Depends(get_db)]
+):
+    _service = UserService(session)
+    return await _service.login(data)
+
+@router.put("/{username}",
+            status_code=HTTPStatus.OK,
+            response_model=UserDBResponse)
 async def update_user(
     data: UserCreate,
     username: str,
-    session: AsyncSession = Depends(get_db)
+    session: Annotated[AsyncSession, Depends(get_db)]
 ):
     _service = UserService(session)
     return await _service.update(username, data)
 
 # DELETE #######################################################################
 
-@router.delete("/{username}", status_code=HTTPStatus.NO_CONTENT)
+@router.delete("/{username}",
+               status_code=HTTPStatus.NO_CONTENT)
 async def delete_user(
     username: str,
-    session: AsyncSession = Depends(get_db),
+    session: Annotated[AsyncSession, Depends(get_db)],
 ):
     _service = UserService(session)
     await _service.delete(username)
-
-################################################################################
-
-@router.post("/login", status_code=HTTPStatus.OK, response_model=UserDBResponse)
-async def login(
-        data: UserLogin,
-        session: AsyncSession = Depends(get_db)
-):
-    _service = UserService(session)
-    return await _service.login(data)
